5901ecda12fa7294291201deb651a3f304eaa55d
IsaiahFlax <flax.isaiah@gmail.com>
2020-03-16 12:53:12 -0600

    refactoring MVP

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -1,8 +1,8 @@
 import useLocalStorage from './useLocalStorage'
 import {useEffect} from 'react'
 
-const useDarkMode = (mode) => {
-    const [darkMode, setDarkMode] = useLocalStorage('dark-mode', mode)
+const useDarkMode = (boolean) => {
+    const [darkMode, setDarkMode] = useLocalStorage('dark-mode', boolean)
 
     useEffect(() => {
         darkMode
